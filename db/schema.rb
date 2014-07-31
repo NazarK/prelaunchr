7824cb20104ad698d890f93f4ced07fcec707d41
Nazar Kuliyev <nazar.kuliev@gmail.com>
2014-07-31 15:10:29 +0600

    added organization model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130312045541) do
+ActiveRecord::Schema.define(:version => 20140731090744) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -69,12 +69,19 @@ ActiveRecord::Schema.define(:version => 20130312045541) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "organizations", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "referral_code"
     t.integer  "referrer_id"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.integer  "organization_id"
   end
 
 end
